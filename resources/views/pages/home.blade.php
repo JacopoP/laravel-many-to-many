e4e56d7a4073c2391d12bc8d3e66fb105b1db73d
Jac22 <jacopo.prudentino@gmail.com>
2023-02-14 18:21:36 +0100

    Add tyoplogy to product info

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,13 +10,15 @@
             <h3>Products: {{$category->products->count()}}</h3>
     
             @foreach($category->products as $product)
-                <h4>{{$product->name}}</h4>
+                <h4 class="mt-1">{{$product->name}}</h4>
                 <div>
                     {{$product->code}}<br>
                     Price: {{$product->price}}<br>
                     @isset($product->weight)
-                        Weight: {{$product->weight}}
+                        Weight: {{$product->weight}}<br>
                     @endisset
+                    Typology: {{$product->typology->name}}<br>
+                    Digital: {{$product->typology->digital ? 'Yes' : 'No'}}
                 </div>
             @endforeach
             <hr>
