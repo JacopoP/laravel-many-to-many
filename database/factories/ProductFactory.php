9b59e64ca704e231be136878d4a92aaa44aae1d7
Jac22 <jacopo.prudentino@gmail.com>
2023-02-14 17:34:04 +0100

    Completed factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'code'=> fake()->unique()->regexify('[A-Z0-9]{8}'),
+            'name'=> fake()->unique()->word(3, false),
+            'description'=> fake()->boolean()
+                        ? fake()->text() 
+                        : NULL,
+            'price'=> fake()->randomFloat(2, 1, 9999.99),
+            'weight'=> fake()->fake()->boolean()
+                        ? fake()->numberBetween(2, 1000) 
+                        : NULL,
+        ];
+    }
+}
---
+++ database/factories/TypologyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Typology>
+ */
+class TypologyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'code'=> fake()->unique()->regexify('[A-Z0-9]{8}'),
+            'name'=> fake()->unique()->word(3, false),
+            'digital'=> fake()->boolean(),
+        ];
+    }
+}
