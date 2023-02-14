e1e53fae6a1bbebc9bd409e0da0e4a1595e99fb7
Jac22 <jacopo.prudentino@gmail.com>
2023-02-14 17:10:02 +0100

    Done migration files

--- database/migrations/2023_02_14_153251_create_category_product_table.php
+++ database/migrations/2023_02_14_153251_create_category_product_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('category_product', function (Blueprint $table) {
-            $table->id();
             $table->timestamps();
         });
     }
