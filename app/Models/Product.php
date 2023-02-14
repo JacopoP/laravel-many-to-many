3aba8c2eea8839cb7a276d9e72b66eec1736f019
Jac22 <jacopo.prudentino@gmail.com>
2023-02-14 16:59:05 +0100

    Completed model files

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'description',
+        'price',
+        'weight',
+    ];
+
+    public function typology(){
+        return $this->belongsTo(Typology::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
--- app/Models/Typology.php
+++ app/Models/Typology.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Typology extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'digital', 
+    ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
